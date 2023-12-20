98fed5b808cc418a79cc0eafeb2650780b319e7b
Refila Dyah Ghizanda Wardoyo <refilaghizanda@gmail.com>
2023-12-21 00:37:09 +0700

    update

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// mendefinisikan kelas Payment
 class Payment extends Model
 {
     use HasFactory;
