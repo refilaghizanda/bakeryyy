ca48e6ee7937464733c44219e90f59eb988a3927
Refila Dyah Ghizanda Wardoyo <refilaghizanda@gmail.com>
2023-12-21 00:08:32 +0700

    update

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -13,10 +13,7 @@ class ResetPasswordController extends Controller
     | Password Reset Controller
     |--------------------------------------------------------------------------
     |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
+    | Controller bertanggung jawab untuk menangani permintaan reset password.
     */
 
     use ResetsPasswords;
